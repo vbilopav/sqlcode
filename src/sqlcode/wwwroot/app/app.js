c064cb62f86580dcd6c44b8cb2e20756a6ab4c3f
vedran <vedran.bilopavlovic@intis.hr>
2018-05-10 15:15:11 +0200

    setup layout

--- src/sqlcode/wwwroot/app/app.js
+++ src/sqlcode/wwwroot/app/app.js
@@ -1,46 +1,13 @@
-define([
-    "template!templates/layout.html",
-    "spa/router",
-    "spa/view-manager",
-    "routes/main"
-], (
-    layout,
-    Router,
-    Manager,
-    routes
-) => {
+define(["text!layout.html"], layout => {
 
-    var 
-        navigation;
-        
-    const
-        router = new Router({
-            routes: routes,
-            navigate: event => navigation.find("#" + event.route.id).addClass("active"),
-            leave: event => !event.route.id || navigation.find("#" + event.route.id).removeClass("active"),
-            error: event => event.router.reveal("/not-found")
-        });
-        
-    return () => {
-        const routerData = router.getData();
-        const app = document.body.find("#app-container").html(
-            layout({
-                home: routerData.filter(item => item.id == "home")[0],
-                templates: routerData.filter(item => item.category == "templates"),
-                modules: routerData.filter(item => item.category == "modules"),
-                dynamic: routerData.filter(item => item.category == "dynamic")
-            })
-        );
-
+    let initApp = () => {
         document.body.find("#loading-screen").remove();
         document.body.find("#loading-screen-script").remove();
+        return app = document.body.find("#app-container");
+    }
 
-        navigation = document.body.find("#navigation");
-
-        router.useViewManager(
-            new Manager(document.body.find("#container"))
-        ).start();
-
-        app.show();
+    return () => {
+        document.title = "sql code";
+        initApp().html(layout).show();
     }
 });
