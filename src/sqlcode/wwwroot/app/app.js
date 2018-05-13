6dce3ce7e3cbe20ea5bf53ceeaecbc29db771d7b
Vedran Bilopavlović <vbilopav@gmail.com>
2018-05-13 10:49:50 +0200

    fix semin colons and commas

--- src/sqlcode/wwwroot/app/app.js
+++ src/sqlcode/wwwroot/app/app.js
@@ -9,15 +9,15 @@ define([
     document.title = "sql code";
 
     const 
-        app = document.body.find("#app-container").html(layout());
-        toolbar = app.children[0];
+        app = document.body.find("#app-container").html(layout()),
+        toolbar = app.children[0],
         main = app.children[1],
         footer = app.children[2],
         left = main.children[0],
         splitter = new Splitter({
-            element: main.children[1], 
-            direction: "h", 
-            resize: 0, 
+            element: main.children[1],
+            direction: "h",
+            resize: 0,
             auto: 2
         }),
         right = main.children[2];
