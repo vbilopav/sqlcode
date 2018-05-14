95402ee61beeb77203c601baf1c26abddedad8e5
Vedran Bilopavlović <vbilopav@gmail.com>
2018-05-14 07:47:35 +0200

    splitter event

--- src/sqlcode/wwwroot/app/app.js
+++ src/sqlcode/wwwroot/app/app.js
@@ -11,16 +11,17 @@ define([
     const 
         app = document.body.find("#app-container").html(layout()),
         toolbar = app.children[0],
-        main = app.children[1],
+
+        contianer = app.children[1],
         footer = app.children[2],
-        left = main.children[0],
+        lpane = contianer.children[0],
         splitter = new Splitter({
-            element: main.children[1],
+            element: contianer.children[1],
             direction: "h",
             resize: 0,
             auto: 2
         }),
-        right = main.children[2];
+        rpane = contianer.children[2];
 
     return () => {
         document.body.find("#loading-screen").remove();
--- src/sqlcode/wwwroot/app/splitter.js
+++ src/sqlcode/wwwroot/app/splitter.js
@@ -12,19 +12,26 @@ define([], () => class {
         this._auto = auto;
         this._offset = null;
         let getValues = () => this._parent.css(this._css).split(" ");
+        let getPos = e => e[this._prop];
         this._element.on("mousedown", e => {
             let value = getValues()[this._resize].replace("px", "");
-            this._offset = value - e[this._prop];
+            this._offset = value - getPos(e);
         });
         document.on("mouseup", () => this._offset = null);
         document.on("mousemove", e => {
             if (this._offset === null) {
                 return
             }
-            e.preventDefault();
             let values = getValues();
-            values[this._resize] = e[this._prop] + this._offset + "px";
+            let pos = getPos(e);
+            values[this._resize] = pos + this._offset + "px";
             values[this._auto] = "auto";
+            //console.log(this._parent.getBoundingClientRect());
+            //console.log(e[this._prop]);
+            if (this._parent.getBoundingClientRect().width - e[this._prop] <= 150) {
+                return;
+            } 
+            e.preventDefault();
             this._element.parentElement.css(this._css, values.join(" "));
         });
     }
