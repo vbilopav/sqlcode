9ed2efa2684e977c3ac70569acfe7ab559b3e963
Vedran Bilopavlović <vbilopav@gmail.com>
2018-05-29 18:52:07 +0200

    remove text module

--- src/postinstall.js
+++ src/postinstall.js
@@ -1,4 +1,2 @@
 var fs = require("fs");
-
 fs.copyFileSync("./node_modules/requirejs/require.js", "./sqlcode/wwwroot/libs/require.js");
-fs.copyFileSync("./node_modules/requirejs-text/text.js", "./sqlcode/wwwroot/libs/text.js");
