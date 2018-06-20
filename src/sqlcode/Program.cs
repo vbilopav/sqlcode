50a870dff32dbbcacef9715fcad3a27dc7d7eed5
Vedran Bilopavlović <vbilopav@gmail.com>
2018-06-20 06:40:12 +0200

    add scripts controller, swagger and diagnostics endpoints

--- src/sqlcode/Program.cs
+++ src/sqlcode/Program.cs
@@ -1,57 +1,128 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
+using System;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.Globalization;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace sqlcode
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+    }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => 
-            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+    public class AppConfig
+    {
+        public string SwaggerTitle { get; set; } = "sqlcode API";
+        public string SwaggerVersion { get; set; } = "v1";
+        public string SwaggerEndpoint { get; set; } = $"/swagger/v1/swagger.json";
+        public string DiagnosticPath { get; set; } = "/diagnostic";
     }
 
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IServiceCollection services;
+        private readonly IConfiguration config;
+        private AppConfig appConfig;
+
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            this.config = config;
+            appConfig = new AppConfig();
+            config.Bind("app", appConfig);
         }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            this.services = services;
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(appConfig.SwaggerVersion,
+                    new Info
+                    {
+                        Title = appConfig.SwaggerTitle,
+                        Version = appConfig.SwaggerVersion
+                    }
+                );
+            });
+
             //services.AddTransient()
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage().UseMvc().UseDefaultFiles().UseStaticFiles();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint(
+                        appConfig.SwaggerEndpoint,
+                        $"{appConfig.SwaggerTitle} {appConfig.SwaggerVersion}"
+                    );
+                });
+                app.UseDiagnosticPage(env, this.services, config, appConfig.DiagnosticPath,
+                    Tuple.Create("AppConfig", appConfig as object));
             }
             else
             {
                 throw new NotImplementedException("Not ready for production yet!");
             }
+        }
+    }
+
+    public static class DiagnosticPageExtensions
+    {
+        public static IApplicationBuilder UseDiagnosticPage(
+            this IApplicationBuilder app,
+            IHostingEnvironment envirment,
+            IServiceCollection services,
+            IConfiguration config,
+            string path = "/diagnostic",
+            Tuple<string, object> extraInfo = null)
+        {
+            app.Map(path, builder => builder.Run(async context =>
+            {
+                context.Response.ContentType = "application/json";
+
+                var result = new Dictionary<string, object>
+                {
+                    { extraInfo?.Item1 ?? "extraInfo is not provided", extraInfo?.Item2 ?? "-" },
+                    { "HostingEnvironment", envirment },
+                    { "Actions", app.ApplicationServices
+                        .GetRequiredService<IActionDescriptorCollectionProvider>()
+                        .ActionDescriptors
+                        .Items
+                        .Select(item => new { name = item.DisplayName, item.AttributeRouteInfo.Template }) },
+                    { "Culture", new { CultureInfo.CurrentCulture, CultureInfo.CurrentUICulture  } },
+                    { "Services", services.
+                        Where(item => !item.ServiceType.FullName.StartsWith("Microsoft") && !item.ServiceType.FullName.StartsWith("System")).
+                        Select(item => new
+                        {
+                            lifetime = item.Lifetime.ToString(),
+                            type = item.ServiceType.FullName,
+                            implementation = item.ImplementationType?.FullName
+                        })
+                    },
+                    { "Configuration", config.AsEnumerable() }
+                };
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(result, Formatting.Indented));
+            }));
+            return app;
         }
     }
 
--- src/sqlcode/wwwroot/app/services/script-service.js
+++ src/sqlcode/wwwroot/app/services/script-service.js
@@ -5,7 +5,7 @@ define([], () => {
 
     return {
         save: (id, type, {viewState, content, title}) => {
-            console.log("save: ", id, type, title);
+            console.log("save: ", id, type, title, viewState, content);
             store[type + id] = {
                 type: type,
                 viewState: viewState,
